bef25cdb474370865cd6f6a1ce04a21281bbc309
Abhishek Maheshwari <abhishek27082000@gmail.com>
2020-09-16 02:04:30 +0530

    popular movie

--- app/src/main/java/android/example/tmdbclientapp/view/MainActivity.java
+++ app/src/main/java/android/example/tmdbclientapp/view/MainActivity.java
@@ -3,7 +3,12 @@ package android.example.tmdbclientapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.example.tmdbclientapp.R;
+import android.example.tmdbclientapp.model.MovieDBResponse;
+import android.example.tmdbclientapp.service.MovieDataService;
+import android.example.tmdbclientapp.service.RetrofitInstance;
 import android.os.Bundle;
+
+import retrofit2.Call;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+     getSupportActionBar().setTitle("Popular Movies");
+
+     viewPopularMovies();
+
+    }
+
+    public void viewPopularMovies(){
+
+        MovieDataService movieDataService = RetrofitInstance.getService();
+         Call<MovieDBResponse> movieDBResponse = movieDataService.getPopularMovies(this.getString(R.string.apiKey));
+
     }
 
 
