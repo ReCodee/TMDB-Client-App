a1f942f6044f5ef3e2486d67b91dd6f30fdf264a
Abhishek Maheshwari <abhishek27082000@gmail.com>
2020-09-16 15:49:41 +0530

    Swipe Refresh layout

--- app/src/main/java/android/example/tmdbclientapp/view/MainActivity.java
+++ app/src/main/java/android/example/tmdbclientapp/view/MainActivity.java
@@ -2,17 +2,32 @@ package android.example.tmdbclientapp.view;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.content.res.Configuration;
 import android.example.tmdbclientapp.R;
+import android.example.tmdbclientapp.model.Movie;
 import android.example.tmdbclientapp.model.MovieDBResponse;
 import android.example.tmdbclientapp.service.MovieDataService;
 import android.example.tmdbclientapp.service.RetrofitInstance;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private MovieAdapter adapter;
+    private ArrayList<Movie> arrayList;
+    private SwipeRefreshLayout swipeRefreshLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,16 +35,46 @@ public class MainActivity extends AppCompatActivity {
      getSupportActionBar().setTitle("Popular Movies");
 
      viewPopularMovies();
-
+      swipeRefreshLayout = findViewById(R.id.swipe);
+      swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+          @Override
+          public void onRefresh() {
+              viewPopularMovies();
+          }
+      });
     }
 
     public void viewPopularMovies(){
 
         MovieDataService movieDataService = RetrofitInstance.getService();
          Call<MovieDBResponse> movieDBResponse = movieDataService.getPopularMovies(this.getString(R.string.apiKey));
+               movieDBResponse.enqueue(new Callback<MovieDBResponse>() {
+                   @Override
+                   public void onResponse(Call<MovieDBResponse> call, Response<MovieDBResponse> response) {
+                       MovieDBResponse movieDBResponse1 = response.body();
+                       arrayList =(ArrayList<Movie>) movieDBResponse1.getResults();
+                       showOnRecyclerView();
+                   }
 
+                   @Override
+                   public void onFailure(Call<MovieDBResponse> call, Throwable t) {
+
+                   }
+               });
     }
 
+    public void showOnRecyclerView(){
+        recyclerView = findViewById(R.id.rvMovies);
+        adapter = new MovieAdapter(this, arrayList);
+        if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        }else {
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        }
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
 
 
 }
---
+++ app/src/main/java/android/example/tmdbclientapp/view/MovieAdapter.java
@@ -0,0 +1,76 @@
+package android.example.tmdbclientapp.view;
+
+import android.content.Context;
+import android.example.tmdbclientapp.R;
+import android.example.tmdbclientapp.model.Movie;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>{
+
+    private Context context;
+    private ArrayList<Movie> movieArrayList;
+
+    public MovieAdapter(Context context, ArrayList<Movie> movieArrayList) {
+        this.context = context;
+        this.movieArrayList = movieArrayList;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_list_item,parent,false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+
+        holder.movieTitle.setText(movieArrayList.get(position).getOriginalTitle());
+        holder.rate.setText(Double.toString(movieArrayList.get(position).getVoteAverage()));
+
+        String imagePath="https://image.tmdb.org/t/p/w500"+movieArrayList.get(position).getPosterPath();
+
+        Glide.with(context)
+                .load(imagePath)
+                .placeholder(R.drawable.loading)
+                .into(holder.movieImage);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieArrayList.size();
+    }
+
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+
+
+        public TextView movieTitle, rate;
+        public ImageView movieImage;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+
+            movieImage = (ImageView) itemView.findViewById(R.id.ivMovie);
+            rate = (TextView) itemView.findViewById(R.id.tvRating);
+            movieTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+
+
+        }
+    }
+}
